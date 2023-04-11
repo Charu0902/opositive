6519a17f0e61bbe27d7725200b5f0773d6beddef
Charu0902 <charu.g@obbserv.com>
2023-04-11 11:01:04 +0530

    no index

--- pages/_app.js
+++ pages/_app.js
@@ -86,6 +86,7 @@ const organizationschema =   {
       <Head>
       <link rel="icon" href="/favicon.png"/>
         {/* you can add metadata here, for all pages */}
+        <meta name="robots" content="noindex,nofollow"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossOrigin="anonymous" />
       </Head>
       <script
